9bee3c6ec9d3230cabe99c06e833e10862935181
hoanglong236 <hahahalalala236@gmail.com>
2026-02-07 18:09:51 +0700

    Thêm thư mục templates và static; tạo trang Home, About với Jinja block

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<p>Hello, World!</p>"
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
