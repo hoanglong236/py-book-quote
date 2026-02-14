a34fcb92d470ddf8f6570ab5da5b624329522cbb
hoanglong236 <hahahalalala236@gmail.com>
2026-02-14 11:38:31 +0700

    Xóa route và template About, cập nhật layout base

--- views/__init__.py
+++ views/__init__.py
@@ -1,3 +1,2 @@
 from .home import home_bp
-from .about import about_bp
 from .auth import auth_bp
--- views/about.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-about_bp = Blueprint('about', __name__)
-
-@about_bp.route('/about')
-def index():
-    return render_template('about.html')
