a091e1f647014469d0a355fe38916cbdb8317744
hoanglong236 <hahahalalala236@gmail.com>
2026-03-07 10:26:44 +0700

    Cập nhật UI Login/Register và giả lập xử lý xác thực

--- views/auth.py
+++ views/auth.py
@@ -1,11 +1,22 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 
 auth_bp = Blueprint('auth', __name__)
 
-@auth_bp.route('/login')
+@auth_bp.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        # Handle login logic...
+        return redirect(url_for('home.index'))
+
     return render_template('auth/login.html')
 
-@auth_bp.route('/register')
+@auth_bp.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        # Handle register logic...
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        return redirect(url_for('auth.login'))
+
     return render_template('auth/register.html')
