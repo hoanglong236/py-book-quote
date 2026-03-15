65007bd70b1afe3b0eb29842449a65733f70273c
hoanglong236 <hahahalalala236@gmail.com>
2026-03-15 17:52:22 +0700

    Thêm cấu hình SECRET_KEY cho project

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import os
 from flask import Flask
 from views import home_bp, auth_bp
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "dev-secret-key")
 
 app.register_blueprint(home_bp)
 app.register_blueprint(auth_bp)
