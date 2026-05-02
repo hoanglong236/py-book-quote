c9162276e5024e0b1ed5f77805fb2f0f4caf0c73
hoanglong236 <hahahalalala236@gmail.com>
2026-05-02 12:28:19 +0700

    Xóa source thừa

--- views/home.py
+++ views/home.py
@@ -7,8 +7,6 @@ home_bp = Blueprint("home", __name__)
 @home_bp.route("/")
 def index():
     user_id = session.get("user_id")
-    if not user_id:
-        return redirect(url_for("auth.login"))
 
     books = get_all_books(user_id)
     return render_template("home.html", books=books)
